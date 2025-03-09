488f3e76ceda7636c7a3d973af9d308e43b56a60
Romain <romain.garry@etud.univ-paris8.fr>
2025-03-09 18:23:19 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,13 +32,25 @@ def save_weekly_users():
     with open("weekly_users.json", "w") as file:
         json.dump(list(users_to_mention), file, indent=4)
 
+def load_channel_id():
+    try:
+        with open("channel.json", "r") as file:
+            data = json.load(file)
+            return data.get("channel_id", 0)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return 0
+
+def save_channel_id(channel_id):
+    with open("channel.json", "w") as file:
+        json.dump({"channel_id": channel_id}, file, indent=4)
+
 intents = discord.Intents.default()
 intents.members = True
 intents.reactions = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 tree = bot.tree
 
-CHANNEL_ID = 0
+CHANNEL_ID = load_channel_id()
 MENTIONED_USERS = load_users()
 users_to_mention = load_weekly_users()
 users_who_reacted = set()
@@ -89,6 +101,7 @@ async def setchannel(interaction: discord.Interaction, channel: discord.TextChan
     print(f"✅ {interaction.user} a défini le channel sur {channel}")
     global CHANNEL_ID
     CHANNEL_ID = channel.id
+    save_channel_id(CHANNEL_ID)
     await interaction.response.send_message(f"✅ Channel défini sur {channel.mention} !", ephemeral=True)
 
 # Commande pour ajouter un utilisateur à la liste des mentions
@@ -103,6 +116,29 @@ async def adduser(interaction: discord.Interaction, member: discord.Member):
         await interaction.response.send_message(f"✅ {member.mention} ajouté aux mentions !", ephemeral=True)
     else:
         await interaction.response.send_message(f"⚠️ {member.mention} est déjà dans la liste !", ephemeral=True)
+
+# Commande pour ajouter un utilisateur à la liste des mentions de cette semaine
+@bot.tree.command(name="addweekuser", description="Ajoute un utilisateur à la liste des mentions de cette semaine")
+async def adduser(interaction: discord.Interaction, member: discord.Member):
+    print(f"✅ {interaction.user} a ajouté {member} aux mentions de cette semaine")
+    if member.id not in users_to_mention:
+        users_to_mention.add(member.id)
+        save_weekly_users()
+        await interaction.response.send_message(f"✅ {member.mention} ajouté aux mentions de cette semaine!", ephemeral=True)
+    else:
+        await interaction.response.send_message(f"⚠️ {member.mention} est déjà dans la liste des mentions de cette semaine!", ephemeral=True)
+
+# Commande pour retirer un utilisateur à la liste des mentions de cette semaine
+@bot.tree.command(name="delweekuser", description="Retire un utilisateur à la liste des mentions de cette semaine")
+async def adduser(interaction: discord.Interaction, member: discord.Member):
+    print(f"✅ {interaction.user} a ajouté {member} aux mentions de cette semaine")
+    if member.id in users_to_mention:
+        users_to_mention.remove(member.id)
+        save_weekly_users()
+        await interaction.response.send_message(f"✅ {member.mention} retiré des mentions de cette semaine!", ephemeral=True)
+    else:
+        await interaction.response.send_message(f"⚠️ {member.mention} n'est pas dans la liste des mentions de cette semaine!", ephemeral=True)
+
 
 # Commande pour lister les utilisateurs qui veulent etre mentionnés
 @bot.tree.command(name="listuser", description="Liste des personnes mentionnées chaque semaine")
@@ -137,9 +173,9 @@ async def check_time():
     now = datetime.now()
     if now.hour == 18 and now.minute == 0: # 18:00
         await send_daily_message()
-    if now.weekday() == 6 and now.hour == 18 and now.minute == 0: # Dimanche 18:00
-        await send_last_day_message()
     if now.weekday() == 6 and now.hour == 20 and now.minute == 0: # Dimanche 20:00
+        await send_last_day_message()
+    if now.weekday() == 6 and now.hour == 20 and now.minute == 30: # Dimanche 21:00
         await send_congrats_message()
     if now.weekday() == 0 and now.hour == 0 and now.minute == 0: # Lundi 00:00
         reset_mentions()
@@ -159,23 +195,23 @@ async def send_daily_message():
     channel = bot.get_channel(CHANNEL_ID)
     if users_to_mention:
         mentions = " ".join([f"<@{user_id}>" for user_id in users_to_mention])
-        message = await channel.send(f"📢 Rappel quotidien ! \n {mentions}")
+        message = await channel.send(f"📢 Rappel quotidien ! 📢\n {mentions}\n Réagissez avec ✅ pour confirmer que vous avez ajouté vos candidatures.")
         await message.add_reaction("✅")
     elif not congrats_sent:
-        await channel.send("🥳 Bien joué la TEAM !")
+        await channel.send("🥳 Bien joué la TEAM ! 🥳\n Vous avez tous ajouté vos offres d'emplois sur iziA !!")
         congrats_sent = True
 
 # Envoyer le message de félicitations le dimanche
 async def send_congrats_message():
     channel = bot.get_channel(CHANNEL_ID)
     mentions = " ".join([f"<@{user_id}>" for user_id in users_who_reacted])
-    await channel.send(f"🎉 Félicitations à {mentions} pour leur participation !")
+    await channel.send(f"🎉 Félicitations à {mentions} pour leur participation ! 🎉")
 
 # Envoyer le dernier message d'alerte le dimanche
 async def send_last_day_message():
     channel = bot.get_channel(CHANNEL_ID)
     mentions = " ".join([f"<@{user_id}>" for user_id in users_to_mention])
-    await channel.send(f"📢 Dernier jour pour ajouter vos offres ! \n{mentions}")
+    await channel.send(f"📢 Dernier jour pour ajouter vos offres ! 📢\n{mentions}")
 
 # Événement déclenché quand un utilisateur réagit à un message
 @bot.event
